53219ead995988a8f0119d816bb89cae19f1a8e8
Umair Jibran <umairjibran7@gmail.com>
2020-08-09 23:41:47 +0500

    developed assignment tile

--- lib/screens/course_detail_screen.dart
+++ lib/screens/course_detail_screen.dart
@@ -1,3 +1,5 @@
+import 'package:canvasteacher/data/courses.dart';
+import 'package:canvasteacher/widgets/assignment_tile.dart';
 import 'package:flutter/material.dart';
 
 class CourseDetailScreen extends StatelessWidget {
@@ -6,11 +8,42 @@ class CourseDetailScreen extends StatelessWidget {
   static const String route = "/courseDetail";
   @override
   Widget build(BuildContext context) {
+    dynamic course =
+        subjects.where((element) => element['id'] == courseID).toList();
+    String courseName = course[0]['subjectName'];
+    // String courseCode = course[0]['subjectCode'];
+    Color courseColor = course[0]['color'];
+
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: courseColor,
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Assigments"),
+            Text(
+              courseName,
+              style: TextStyle(fontSize: 12),
+            )
+          ],
+        ),
+      ),
       body: Container(
-        child: Center(
-          child: Text("Course Detail Screen for $courseID"),
+        padding: EdgeInsets.only(left: 10, top: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Labs and Assignments",
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 17,
+              ),
+            ),
+            AssignmentTile(
+              color: courseColor,
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/widgets/assignment_tile.dart
@@ -0,0 +1,97 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+
+class AssignmentTile extends StatelessWidget {
+  AssignmentTile({this.color, this.assignmentCode});
+  final Color color;
+  final assignmentCode;
+  final haveDue = false;
+  final dueDate = "Jun 20";
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Colors.black12,
+      onTap: () {},
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.14,
+        padding: EdgeInsets.only(top: 12),
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width * 0.1,
+                child: Stack(
+                  children: [
+                    Icon(
+                      Icons.insert_drive_file,
+                      size: 40,
+                      color: color,
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Container(
+                          padding: EdgeInsets.only(top: 2.0, left: 2.5),
+                          color: Colors.white,
+                          child: Icon(
+                            haveDue ? Icons.cloud_done : Icons.cloud_off,
+                            color: haveDue ? Colors.green : Colors.grey,
+                            size: 15,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AutoSizeText(
+                    "Cell Biology: The Quickest Route for Protein Transport",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 2,
+                    maxFontSize: 26,
+                    minFontSize: 12,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Due $dueDate at 11:59 PM",
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 15,
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      border: Border.all(color: color),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 6.0,
+                      vertical: 3.0,
+                    ),
+                    child: Text(
+                      "2 Needs Grading",
+                      style: TextStyle(color: color),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
