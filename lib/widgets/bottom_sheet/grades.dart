2dae706385ae863dc3e8fd75075c1eeab66e8cd0
Umair Jibran <umairjibran7@gmail.com>
2020-08-15 23:29:11 +0500

    completed first tab of bottom sheet

--- lib/widgets/bottom_sheet/grades.dart
+++ lib/widgets/bottom_sheet/grades.dart
@@ -4,7 +4,49 @@ class BottomSheetGradeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      children: [
+        ListTile(
+          title: Text(
+            "Grade",
+            style: TextStyle(
+              fontSize: 21,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          trailing: IconButton(
+            icon: Icon(
+              Icons.add,
+              color: Colors.blue,
+            ),
+            onPressed: () {},
+          ),
+        ),
+        ListTile(
+          title: Text(
+            "Rubric",
+            style: TextStyle(
+              fontSize: 21,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: Text(
+            "0 out of 20.0",
+            style: TextStyle(color: Colors.grey),
+          ),
+          trailing: FlatButton(
+            onPressed: () {},
+            child: Text(
+              "SAVE",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ),
+        ),
+        Text("Detailed Knowledge of atleast one"),
+      ],
     );
   }
 }
